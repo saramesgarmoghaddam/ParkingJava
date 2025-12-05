fe9111322c40de6195f7d592a832b72674afc6fe
sara moghaddam <sarmem2006@gmail.com>
2025-12-05 17:44:42 +0330

    update StackCar class for parking cars

--- src/model/StackCar.java
+++ src/model/StackCar.java
@@ -1,4 +1,45 @@
 package model;
 
 public class StackCar {
+    private Node top;
+    private int size;
+    private int capacity;
+    
+    public StackCar(int capacity){
+        this.top = null;
+        this.size = 0;
+        this.capacity = capacity;
+    }
+    
+    public void push(CarInformation car){
+        if(size == capacity){
+            throw new IllegalStateException("Stack is full!");
+        }
+        Node newNode = new Node(car);
+        newNode.next = top;
+        top = newNode;
+        size++;
+    }
+    
+    public  CarInformation pop(){
+        if(top == null){
+            throw new IllegalStateException("stack is empty!");
+        }
+        CarInformation car = top.car;
+        top = top.next;
+        size--;
+        return car;
+    }
+    
+    public boolean isEmpty(){
+        return size == 0;
+    }
+    
+    public boolean isFull(){
+        return size == capacity;
+    }
+    
+    public int getSize(){
+        return size;
+    }
 }
